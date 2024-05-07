a867471d2bb64a077d99e2bdd3699c6f6fe9a8d6
BurkeyLai <imugly1029@gmail.com>
2024-05-07 22:16:06 +0800

    Add DaikinAC project

--- BlinkOuterLED/BlinkOuterLED.ino
+++ BlinkOuterLED/BlinkOuterLED.ino
@@ -1,8 +1,9 @@
-#include <IRremote.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
 
 #define IR_LED_PIN D1 // 將 LED 連接到 NodeMCU 的 D1 腳位
 
-IRsend irsend;
+IRsend irsend(4);
 
 void setup() {
   Serial.begin(115200);
@@ -12,6 +13,7 @@ void setup() {
 void loop() {
   // 控制外接 LED 閃爍
   digitalWrite(IR_LED_PIN, HIGH);
+  // irsend.sendNEC(0xF7C03F, 32);
   delay(1000); // 延遲一秒
   digitalWrite(IR_LED_PIN, LOW);
   delay(1000); // 再次延遲一秒
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ArduinoHub
 ## BlinkOuterLED
 ![circuts](BlinkOuterLED/circuts.jpeg)
+
+## DaikinAirConditioner
+### TurnOnOffDaikinAC
+* [Reference wiring](https://github.com/crankyoldgit/IRremoteESP8266/wiki#ir-sending)
+![circuts](DaikinAirConditioner/TurnOnOffDaikinAC/circuts.jpeg)
+
+## FirstServer
+* [Reference](https://blog.jmaker.com.tw/esp8266-esp01/)
